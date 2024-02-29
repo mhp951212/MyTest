06c159d78498bf586bb71a3c794ab97d13d44fc3
mhp951212 <mei4577579>
2024-02-29 10:46:21 +0800

    合并成一个1

--- someCommonCode/paomadeng/test.ts
+++ someCommonCode/paomadeng/test.ts
@@ -4,5 +4,13 @@
  * @description: 文件描述
  */
 class a{
-    sssss
+    constructor() {
+
+    }
+    sssss;
+    public t(){
+     let  a_1 = "测试1";
+     let  a_2 = "测试2";
+     let  a_3 = "测试3";
+    }
 }
